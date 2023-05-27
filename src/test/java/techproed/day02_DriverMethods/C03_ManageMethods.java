ea963aeb0b53c97968eb7f98021e3b95dfeb8665
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-05-27 22:40:23 +0300

    ekleme

--- src/test/java/techproed/day02_DriverMethods/C03_ManageMethods.java
+++ src/test/java/techproed/day02_DriverMethods/C03_ManageMethods.java
@@ -28,6 +28,13 @@ public class C03_ManageMethods {
         Thread.sleep(3000);
         //Browser'i maximize yapalim.
         driver.manage().window().maximize();
+    /*
+    Bir web sitesine gittiğimizde browser default olarak bir boyutta gelir. Ve açılan browser'daki
+    webElementlere browser maximize olmadığı için ulaşamayabiliriz. Dolayısıyla browser'ı açtıktan sonra
+    maximize yaparsak driver bütün webelementleri görür ve rahatlikla müdehale edebilir. Bu yüzden
+    browser'ı açtıktan sonra ilk olarak driver.manage().window().maximize(); yapmamız bizim webelementlere
+    ulaşmada işimizi kolaylaştırır. Böylelikle fail almamış oluruz
+    */
 
         Thread.sleep(3000);
         //Techproeducation sayfasına gidelim
@@ -36,6 +43,7 @@ public class C03_ManageMethods {
 
         //Browser'in konumunu yazdiralim
         System.out.println("Sayfanin konumu : "+driver.manage().window().getPosition());
+        Thread.sleep(3000);
 
 
         //Browser'in boyutlarini yazdiralim
@@ -43,19 +51,19 @@ public class C03_ManageMethods {
         Thread.sleep(3000);
 
 
-        Thread.sleep(3000);
         //Sayfayi minimize yapalim.(asagi atalim)
         driver.manage().window().minimize();
+        Thread.sleep(3000);
 
 
         //Sayfayi FullScreen yapalim.
         driver.manage().window().fullscreen();
         Thread.sleep(3000);
 
-        driver.manage().window().setPosition(new Point(50,50));//hedef konumu belirtmem gerek x,y olarak. icine new point degiyp yeni degerleri belirtmem gerkir.
-                                                                     //bu sayede istedimiz konuma getirir.
-        driver.manage().window().setSize(new Dimension(600,600));//istedimiz olculerde yazdirmayi saglar.
 
+        driver.manage().window().setPosition(new Point(50,50));//hedef konumu belirtmem gerek x,y olarak. icine new point degiyp yeni degerleri belirtmem gerkir.
+        Thread.sleep(3000);                                                             //bu sayede istedimiz konuma getirir.
+        driver.manage().window().setSize(new Dimension(600,600));//istedimiz olculerde yazdirmayi saglar.
 
 
         Thread.sleep(3000);
