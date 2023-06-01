2c596aabe4ff6a4d0d010a236b59e3f4f8a84dad
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-06-01 13:33:27 +0300

    ekleme

--- src/test/java/techproed/day05_XpathCssSelector/C02_CssSelectors.java
+++ src/test/java/techproed/day05_XpathCssSelector/C02_CssSelectors.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class C02_CssSelectors {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
@@ -16,10 +16,12 @@ public class C02_CssSelectors {
 
         //https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        Thread.sleep(2000);
 
 
         //Add Element butonuna basin
         driver.findElement(By.cssSelector("button[onclick ='addElement()']")).click();
+        Thread.sleep(2000);
 
 
         //Delete butonu'nun gorunur oldugunu test edin
@@ -29,17 +31,36 @@ public class C02_CssSelectors {
         }else {
             System.out.println("Delete butonu GORUNUR DEGIL");
         }
+        Thread.sleep(2000);
 
         //Delete butonuna basin
+         deleteButton .click();
+        Thread.sleep(2000);
 
 
         //"Add/Remove Elements" yazisinin gorunur oldugunu test edin
+        WebElement AddRemoveElements =  driver.findElement(By.cssSelector("h3"));//css text ile calismadigi icin, herhangi bir attribute olmadigi icin sadece tag ile islem yaptik.
+        if(AddRemoveElements.isDisplayed()){
+            System.out.println("AddRemoveElements GORUNUR");
+        }else {
+            System.out.println("AddRemoveElements GORUNUR DEGIL");
+        }
+        Thread.sleep(2000);
 
 
         //Sayfayı kapatın
-       // driver.close();
+        driver.close();
 
 
+        /*
+
+        Xpath ve cssSlelector arsindaki farklar:
+        - Xpath text ile calisir.
+        - cssSelector text ile calismaz
+        - Xpath indekse gore arama yapabilir. Daha fazla kombinasyona sahiptir.
+        - cssSelector indekse gore arama yapamaz ama daha hizli calisir.
+
+         */
 
 
 
