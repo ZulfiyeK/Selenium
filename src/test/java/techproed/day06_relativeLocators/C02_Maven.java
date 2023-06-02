6b2af077d10891cbfc36947e8174a05c67225de4
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-06-02 22:02:30 +0300

    yeni konu

---
+++ src/test/java/techproed/day06_relativeLocators/C02_Maven.java
@@ -0,0 +1,13 @@
+package techproed.day06_relativeLocators;
+
+public class C02_Maven {
+    /*
+    Maven bir java derleme aracidir(build tool)
+    Maven proje veotomasyon ve yonetim aracidir(automation and management tool)
+    Maven , konfigurasyon icin pom.xml dosyasini kullanir.
+    Bu dosya projenin insasi, reporlamasi ve dokumantasyonu icin gerkli butun builgileri icerir(dependencies, plugins)
+
+
+    pom.xml --> Maven projesindeki en onemli dosyadir.
+     */
+}
